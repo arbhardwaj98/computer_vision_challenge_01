82fac1ec69789b37bc8c72dd82b8743199235fe4
Arjun Bhardwaj <arbhardwaj98@gmail.com>
2022-05-16 19:08:04 +0200

    Solution

--- README.md
+++ README.md
@@ -9,3 +9,27 @@ In the query_images folder, there are 1,331 image consisting of various spatial 
 In this challenge you have to find the best match between the query images and the single aerial image. To further clarify, you have to pick one query image which matches the most with the aerial image on a pixel by pixel basis. This means that you have to iterate through each image and find the image that most closely aligns with the aerial image on a pixel by pixel basis. We show you the right answer in merged.png. Once you find the best match, please send us the image filename that you think is the best match and the corresponding algorithm that you used to find the best image candidate.
 
 
+### SOLUTION:
+
+The best matching image is `c1e2150520cb798ec63a7e8d0f311e54aba92484.png`. The instructions
+for setting up and running the code are given in the following sections.
+
+#### Setup:
+Create and initialize a conda environment for the project: 
+```
+$ conda create -n cv_challenge_1 python=3.7
+$ conda init cv_challenge_1
+```
+Install the required python packages:
+```
+$ pip install -r requirements.txt
+```
+
+#### Testing:
+To start the query simpy run the following command:
+```
+$ python3 main.py --RAY_verbose_spill_logs=0
+```
+The code saves an overlay of the best matching image on the reference image as `best_match_overlay.png`
+
+
--- main.py
+++ main.py
@@ -11,15 +11,31 @@ def normalize_image(img: np.ndarray) -> np.ndarray:
     :param img: Input image
     :return: Normalized image
     """
-    spatial_axes = tuple(np.arange(img.ndim-1))  # Normalize only along spatial dimensions
+    # Normalize only along spatial dimensions
+    spatial_axes = tuple(np.arange(img.ndim-1))
+
+    # Compute the standard deviation of the pixel values for each channel
     img_std = np.std(img, axis=spatial_axes)
+
+    # Normalize the image
     img_norm = img / img_std
 
     return img_norm
 
 
 @ray.remote
-def get_pixel_dist(query_img_path, normalized_reference_img, dist_func="dice"):
+def get_pixel_dist(
+        query_img_path: str,
+        normalized_reference_img: np.ndarray,
+        dist_func: str = "dice"
+) -> float:
+    """
+    Find the mean pixel-wise distance between the query and the reference images
+    :param query_img_path: Path to query image
+    :param normalized_reference_img: Normalized reference image
+    :param dist_func: Function to use for calculating pixel-wise distance
+    :return: Mean pixel-wise distance
+    """
 
     # Load query image as a numpy array
     query_img = cv2.imread(os.path.join(query_image_dir, query_img_path))
@@ -32,30 +48,46 @@ def get_pixel_dist(query_img_path, normalized_reference_img, dist_func="dice"):
 
     # Find pixel-wise distance
     if dist_func == "error":  # Normalized error loss
-        pixel_dist_norm = (
+        pixel_dist = (
             abs(normalized_reference_img[mask] - normalized_query_img)
             /
             (normalized_reference_img[mask] * normalized_query_img + 1)
         ).mean()
 
     elif dist_func == "dice":  # Dice loss
-        pixel_dist_norm = 1 - 2*(
+        pixel_dist = 1 - 2*(
                 (normalized_reference_img[mask] * normalized_query_img)
                 /
-                abs(normalized_reference_img[mask] + normalized_query_img)
+                (normalized_reference_img[mask] + normalized_query_img)
         ).mean()
 
     else:
         raise NotImplementedError
 
-    return pixel_dist_norm
+    return pixel_dist
 
 
 if __name__ == "__main__":
+    """
+    ALGORITHM:
+    
+    This script loads the reference image and iterates over 
+    the query images to find the best match. The best match
+    corresponds to the image with the least pixel-wise distance.
+       
+    A mask is created to remove the blank pixels from the query
+    image. Pixel values are compared only where query image
+    pixels are not blank. The pixel values of the images are 
+    first normalized to account for affine intensity changes. 
+    The function get_pixel_dist finds the mean pixel-wise distance
+    between the normalized reference and normalized query image. 
+    """
 
+    # Initialize ray for multiprocessing
     ray.init()
 
-    output_overlay = True  # Output overlaid image
+    # Outputs overlaid image if set to True
+    output_overlay = True
 
     # Load and normalize the reference image
     reference_img = cv2.imread("reference_image/aerial.png")
@@ -65,12 +97,17 @@ if __name__ == "__main__":
     query_image_dir = "query_images"
     query_img_paths = os.listdir(query_image_dir)
 
+    # Create a list of ray futures
     result_ids = [get_pixel_dist.remote(query_path, normalized_reference_img) for query_path in query_img_paths]
+
+    # Accumulate results from ray futures
     results = ray.get(result_ids)
 
+    # Find the image with minimum pixel-wise distance
     best_match_idx = np.argmin(np.array(results))
     print(f"Best matching image is ", query_img_paths[best_match_idx])
 
+    # Save output image
     if output_overlay:
         output_path = "best_match_overlay.png"
         best_match = cv2.imread(os.path.join(query_image_dir, query_img_paths[best_match_idx]))
